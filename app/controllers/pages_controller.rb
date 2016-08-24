1fa414fef57dbaadf14fc58f1ac3b2e56b71aaa6
titli1981 <madhumishra084@gmailcom>
2016-08-24 19:23:55 -0400

    defined Dynemic Routes by defining kitten method inside controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,19 @@
 class PagesController < ApplicationController
   def welcome
+
+    @header = "Welcome to Madhu's website"
   end
 
   def about
+    @header = "I am at bitamker."
   end
 
   def contest
+    @header = "I am not sure what goes here"
+  end
+  def kitten
+    requested_size = params[:size]
+    @kitten_url = "http://placekitten.com/#{requested_size}"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get "/welcome" => "pages#welcome"
   get "/about" => "pages#about"
   get "/contest" => "pages#contest"
+  get "/kitten/:size" => "pages#kitten"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
