97725c07b798738b787b1277dccd3109bf980c23
titli1981 <madhumishra084@gmailcom>
2016-08-24 18:10:17 -0400

    rendered Contest page by defining route and controller method

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get "/" => "pages#welcome"
   get "/welcome" => "pages#welcome"
   get "/about" => "pages#about"
+  get "/contest" => "pages#contest"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
