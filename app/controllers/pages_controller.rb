2646d8214be7b65837b5e699e32286d602a37bde
titli1981 <madhumishra084@gmailcom>
2016-08-24 19:54:04 -0400

    used before_action to keep code DRY

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
+  before_action :set_kitten_url, only: [:kitten, :kittens]
   def welcome
-
     @header = "Welcome to Madhu's website"
   end
 
@@ -12,8 +12,15 @@ class PagesController < ApplicationController
     @header = "I am not sure what goes here"
   end
   def kitten
-    requested_size = params[:size]
-    @kitten_url = "http://placekitten.com/#{requested_size}"
+
   end
+  def kittens
+
+  end
+  def set_kitten_url
+      requested_size = params[:size]
+      @kitten_url = "http://placekitten.com/#{requested_size}"
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get "/about" => "pages#about"
   get "/contest" => "pages#contest"
   get "/kitten/:size" => "pages#kitten"
+  get "/kittens/:size" => "pages#kittens"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
