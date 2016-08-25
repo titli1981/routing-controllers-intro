41d117e882445e38ccc5ea0401ec040c35c138a6
titli1981 <madhumishra084@gmailcom>
2016-08-24 21:13:49 -0400

    Used conditional redirects

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,17 +9,33 @@ class PagesController < ApplicationController
   end
 
   def contest
-    @header = "I am not sure what goes here"
+    #@header = "I am not sure what goes here"
+    flash[:notice] = "Sorry, the contest has ended "
+    redirect_to "/welcome"
   end
+
   def kitten
-
   end
+
   def kittens
-
   end
+
   def set_kitten_url
       requested_size = params[:size]
       @kitten_url = "http://placekitten.com/#{requested_size}"
+  end
+
+  def secrets
+    #@user_magic_word = params[:magic_word]
+    my_magic_word = "magic"
+    if my_magic_word == params[:magic_word]
+      @result = "the secret is correct"
+    else
+      flash[:alert] = "Sorry, you're not authorized to see that page!"
+      redirect_to "/welcome"
+
+    end
+
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get "/contest" => "pages#contest"
   get "/kitten/:size" => "pages#kitten"
   get "/kittens/:size" => "pages#kittens"
+  get "/secrets/:magic_word" => "pages#secrets"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
